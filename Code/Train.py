00054e2ae2ed762d24b6c4ecfb68c29701e39c68
MinhDTN <96186749+husthunterpy01@users.noreply.github.com>
2023-05-26 03:04:46 +0700

    Add files via upload

--- Code/Train.py
+++ Code/Train.py
@@ -1,51 +1,33 @@
+import torch
+import sys
+import time
+import numpy as np
+import pandas as pd
+
+from torch.utils.data.dataloader import DataLoader
 #Train
+torch.manual_seed(42)
 def training():
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        train_data = TimeSeriesDataset(config, mode='train')
+        train_data = CustomDataset(config, mode='train')
         train_loader = DataLoader(train_data,
-                                  batch_size=config['batch_size'],
-                                  shuffle=True,
-                                  num_workers=config['num_workers'])
+                                  batch_size=128,
+                                  shuffle=True)
 
-        model = create_transformer(d_model=config['d_model'],
-                                    nhead=config['n_head'],
-                                    dff=config['dff'],
-                                    num_layers=config['num_layers'],
-                                    dropout=config['dropout'],
-                                    l_win=config['l_win'])
+        model = Net(d_model=config['d_model'],
+                    nhead=config['n_head'],
+                    noise_level = config['noise_level'],
+                    feature_size = config['feature_size'],
+                    embed_dim = config['embed_dim'],
+                    n_head = configp['n_head'],
+                    num_layers=config['num_layers'],
+                    dropout=config['dropout'],
+                    l_win=config['l_win'])
 
         optimizer = torch.optim.Adam(model.parameters(), lr=config["lr"], weight_decay=config['weight_decay'])
         criterion = nn.MSELoss()
         trainer = ModelTrainer(model, train_loader, criterion, optimizer, device, config)
-
         trainer.train()
 
-        #inference.py
-
-        test_data = TimeSeriesDataset(config, mode='test')
-        test_loader = DataLoader(test_data,
-                                    batch_size=1,
-                                    shuffle=False,
-                                    num_workers=config['num_workers'])
-
-        model.to(device)
-        test_loss = 0.0
-        criterion = nn.MSELoss()
-        test_loss_list = list()
-        pred_list = list()
-        with torch.no_grad():
-            for x, rul in test_loader:
-                out = model(x.to(device).float())
-                loss = torch.sqrt(criterion(out.float(), rul.to(device).float()))
-                test_loss += loss
-                test_loss_list.append(loss)
-                pred_list.append(out.float())
-
-        test_loss_avg = test_loss / len(test_loader)
-        truth_list = [rul.float().item() for x, rul in test_loader]
-        config['truth_list'] = truth_list
-        config['pred_list'] = pred_list
-        config['test_loss_avg'] = test_loss_avg
-        config['test_loss_list_per_id'] = test_loss_list
-        wandb.log({"test_loss_avg": test_loss_avg})
+ 
      
--- Code/Trainer.py
+++ Code/Trainer.py
@@ -15,86 +15,112 @@ from scipy.stats import randint as sp_randint
 from scipy.stats import uniform
 
 # import custom functions and classes
-from utils import EarlyStopping
-from src.data.data_utils import load_train_test_ims, load_train_test_femto
-from model import Net
-from loss import RMSELoss, RMSLELoss, WeibullLossRMSE, WeibullLossRMSLE, WeibullLossMSE
 import h5py
-from src.visualization.visualize_training import (
-    plot_trained_model_results_ims,
-    plot_trained_model_results_femto,
-)
 import argparse
-class RegressionTrainer():
-    def __init__(self,model,train_data,criterion,optimizer,config):
+class ModelTrainer():
+    def __init__(self,model,train_data,val_data,criterion,optimizer,config):
         self.model = model
         self.train_data = train_data
+        self.val_data = val_data
         self.config = config
+        self.train_data = train_data
+        self.train_loss_list = []
+        self.test_loss_list = []
+        self.val_loss_list = []
         self.train_loss_list = list()
         self.min_loss = float('inf')
         self.best_model = None
         self.best_optimizer = None
         self.optimizer = optimizer
         self.criterion = criterion
-        
-    def train_epoch(self,epoch):
+
+# Define the train, test and validation epoch
+    # Train epoch
+    def train_epoch(self,criterion,epoch,opt):
         train_loss = 0.0
         self.model.train()
-        for x, rul in self.train_data:
-            self.mode.zero_grad()
-            out = self.model(input)
-            loss = torch.sqrt(self.criterion(out.float(), rul.to(self.device).float()))
+        for i, data in self.train_data:
+            input1 = data['input'].float().to(self.device)
+            target1 = data['labels'].float().to(self.device)
+    # Pass the processed data into the model
+            self.model.zero_grad()
+            out = self.model(input1)
+            classes = torch.argmax(input1, axis=1)
+            target = torch.argmax(target1, axis=1)
+    #Define the loss with its calculation
+            loss = torch.sqrt(self.criterion(out.float(), target1.float()))
             loss.backward()
-            self.optimizer.step()
+            opt.step()
             train_loss += loss
-            
+    # Formula to calculate the train_los over the total dât
         train_loss = train_loss / len(self.train_data)
+    # Append the loss into the list
         self.train_loss_list.append(train_loss)
-        
-        if train_loss M self.min_loss
-            self.min_loss = train_loss
-            self.best_model = deepcopy(self.model.state.dict())
-            self.best_optimizer = deepcopu(self.optimizer.state_dict())
-            self.best_epoch_in_round = epoch
-            
-     def validate_epoch(self, criterion, epoch):
+        if self.val_data is None:
+            if train_loss < self.min_loss:
+               self.min_loss = train_loss
+               self.best_model = deepcopy(self.model.state_dict())
+               self.best_optimizer = deepcopy(self.optimizer.state_dict())
+               self.best_epoch_in_round = epoch
+        else:
+            validate_epoch(criterion, epoch)
+
+    # Validate epoch
+    def validate_epoch(self, criterion, epoch):
+        val_loss = 0.0
         test_loss = 0.0
         length = 0
-        lengthval = 0
+        length_val = 0
         self.model.eval()
         with torch.no_grad():
             for i, data in enumerate(self.val_data):
-                input = data['input'].float().to(self.device)
-                target = data['labels'].float().to(self.device)
+                input1 = data['input'].float().to(self.device)
+                target1 = data['labels'].float().to(self.device)
                 
-                out = self.model(input)
-                lenght += len(input)
+                out = self.model(input1)
+                length += len(input)
                 classes = torch.argmax(out, axis=1)
-                target_ = torch.argmax(target, axis=1)
-                loss = criterion(out, target)
+                target_ = torch.argmax(target1, axis=1)
+                loss = criterion(out, target1)
                 test_loss += loss.item()
             
             for i, data in enumerate(self.val_set):
-                input = data['input'].float().to(self.device)
-                target = data['labels'].float().to(self.device)
+                input1 = data['input'].float().to(self.device)
+                target1 = data['labels'].float().to(self.device)
                 
-                out = self.model(input)
-                lengthval += len(input)
+                out = self.model(input1)
+                length_val += len(input1)
                 
                 classes = torch.argmax(out, axis=1)
-                target_ = torch.argmax(target, axis=1)
+                target_ = torch.argmax(target1, axis=1)
 
             
-        test_loss = test_loss/lenght
+        test_loss = test_loss/length
         self.test_loss_list.append(test_loss)
-         print('\rEpoch: {}\t| Train_loss: {:.6f}\t| Test_loss: {:.6f}\t|Val accuracy: {:.6f} '.format(
-                                                                                                        epoch, 
-                                                                                                        self.train_loss, 
-                                                                                                        self.train_acc, 
-                                                                                                        test_loss, 
-                                                                                                        test_acc, 
-                                                                                                        val_acc), 
+
+        val_loss = val_loss/length
+        self.val_loss_list.append(val_loss)
+# Print the result
+        print('\rEpoch: {}\t| Train_loss: {:.6f}\t| Test_loss: {:.6f}\t|Val loss: {:.6f} '.format(epoch,
+                                                                                         self.train_loss,
+                                                                                         test_loss,
+                                                                                         val_loss),
+                                                                                        end="\r")
+
                
+# Start the trainning process
+def train(self):
+    self.model.to(self.device)
+
+    for epoch in range(1, self.config['n_epochs'] + 1):
+        self.train_epoch(epoch)
+
+        self.config['train_loss_list'] = self.train_loss_list
+
+def update_config(self):
+    return self.config
+
+
 
                                                                              
     
