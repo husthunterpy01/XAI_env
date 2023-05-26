14d59a9264315c07ce03427685656758d1bcb908
MinhDTN <96186749+husthunterpy01@users.noreply.github.com>
2023-05-26 22:48:49 +0700

    Add files via upload

--- Code/Train.py
+++ Code/Train.py
@@ -29,5 +29,47 @@ def training():
         trainer = ModelTrainer(model, train_loader, criterion, optimizer, device, config)
         trainer.train()
 
- 
-     
+        test_data = CustomDataset(config, mode='test')
+        test_loader = DataLoader(test_data,
+                                  batch_size=128,
+                                  shuffle=True)
+        model.to(device)
+        test_loss = 0.0
+        test_loss_list = list()
+        pred_list = list()
+        with torch.no_grad():
+            for x, rul in test_loader:
+                out = model(x.to(device).float())
+                loss = torch.sqrt(criterion(out.float(), rul.to(device).float()))
+                test_loss += loss
+                test_loss_list.append(loss)
+                pred_list.append(out.float())
+
+        test_loss_avg = test_loss / len(test_loader)
+        config['truth_list'] = truth_list
+        config['pred_list'] = pred_list
+        config['test_loss_avg'] = test_loss_avg
+        config['test_loss_list_per_id'] = test_loss_list
+        wandb.log({"test_loss_avg": test_loss_avg})
+
+
+
+
+
+        vak_data = CustomDataset(config, mode='val')
+        val_loader = DataLoader(val_data,
+                                 batch_size=128,
+                                 shuffle=True)
+        model.to(device)
+        val_loss = 0.0
+        val_loss_list = list()
+        with torch.no_grad():
+            for x, rul in val_loader:
+                out = model(x.to(device).float())
+                loss = torch.sqrt(criterion(out.float(), rul.to(device).float()))
+                val_loss += loss
+                val_loss_list.append(loss)
+        val_loss_avg = val_loss / len(test_loader)
+        config['val_loss_avg'] = test_loss_avg
+        config['val_loss_list_per_id'] = test_loss_list
+        wandb.log({"val_loss_avg": test_loss_avg})
